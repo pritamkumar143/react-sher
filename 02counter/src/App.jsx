9697c1c7eadf3f2f5d4212d492b5875df5d761d3
Pritam Kumar <pritamkunar13@gmail.com>
2024-09-14 00:39:20 +0530

    1st project counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(15);
+
+
+  // let counter = 15
+
+  const addValue = () => {
+    // console.log("clicked", counter);
+    // counter = counter + 1
+    if(counter<20){
+    setCounter((prevCounter) => prevCounter + 1)
+    }
+  };
+
+
+  const removeValue = () =>{
+    if(counter>0){
+      setCounter((prevCounter) => prevCounter - 1);
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >Remove value {counter}</button>
+
+      <p>footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
